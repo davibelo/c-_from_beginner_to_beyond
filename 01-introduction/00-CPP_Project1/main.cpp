6a612c2c93e3b578696b54898bc3bf847c118fd5
davibelo <davibelo@gmail.com>
2021-11-24 21:42:09 -0300

    basics

--- 01-introduction/00-CPP_Project1/main.cpp
+++ 01-introduction/00-CPP_Project1/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int fav_number {100};
+int main()
+{
+    // declaring a integer variable with a initial value of 100
+    int fav_number{100};
+    // writing on console
     cout << "Hello!" << endl;
     cout << "Enter your favorite number: ";
+    // getting information from console
     cin >> fav_number;
-    cout << "Your favorite number is: " << fav_number << endl;
+    // using a conditional statement
+    if (fav_number == 50)
+    {
+        cout << "Amazing! " << fav_number << " is my fav. number too" << endl;
+    }
+    else
+    {
+        cout << "Your favorite number is: " << fav_number << endl;
+    }
     return 0;
 }
